2cf11e00dac992383c47e224d5e1de8fe152f1bc
Mikkel Paulson <git@email.mikkel.ca>
2020-12-04 10:58:48 -0500

    day 4 part 1

---
+++ day4/__init__.py
@@ -0,0 +1,49 @@
+"""https://adventofcode.com/2020/day/4"""
+
+import re
+
+
+def part1(stdin, stdout, stderr):
+    """
+    ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+    byr:1937 iyr:2017 cid:147 hgt:183cm
+
+    iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+    hcl:#cfa07d byr:1929
+
+    hcl:#ae17e1 iyr:2013
+    eyr:2024
+    ecl:brn pid:760753108 byr:1931
+    hgt:179cm
+
+    hcl:#cfa07d eyr:2025 pid:166559648
+    iyr:2011 ecl:brn hgt:59in
+
+    Count the number of valid passports - those that have all required fields.
+    Treat cid as optional. In your batch file, how many passports are valid?
+    """
+
+    passports = [
+        dict(
+            x.split(":")
+            for x in re.split(r"[\n ]", y)
+        )
+        for y in stdin.read().strip().split("\n\n")
+    ]
+
+    valid_passports = 0
+    required_keys = ("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
+
+    def validate(passport):
+        for key in required_keys:
+            if key not in passport:
+                return False
+
+        return True
+
+    for passport in passports:
+        stderr.write(f"{passport}")
+        if validate(passport):
+            valid_passports += 1
+
+    stdout.write(f"{valid_passports}\n")
