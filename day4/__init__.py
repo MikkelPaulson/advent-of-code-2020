130de9cae7e5f6065c5b5aec9540797db44857d9
Mikkel Paulson <git@email.mikkel.ca>
2020-12-04 11:45:57 -0500

    day 4 part 2

--- day4/__init__.py
+++ day4/__init__.py
@@ -26,15 +26,64 @@ def part1(stdin, stdout, stderr):
     passports = parse(stdin)
 
     valid_passports = 0
-    required_keys = ("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
 
     def validate(passport):
-        for key in required_keys:
+        for key in ("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"):
             if key not in passport:
                 return False
 
         return True
 
+    for passport in passports:
+        stderr.write(f"{passport}\n")
+        if validate(passport):
+            valid_passports += 1
+
+    stdout.write(f"{valid_passports}\n")
+
+
+def part2(stdin, stdout, stderr):
+    """
+    byr (Birth Year) - four digits; at least 1920 and at most 2002.
+    iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+    eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+    hgt (Height) - a number followed by either cm or in:
+        If cm, the number must be at least 150 and at most 193.
+        If in, the number must be at least 59 and at most 76.
+    hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+    ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+    pid (Passport ID) - a nine-digit number, including leading zeroes.
+    cid (Country ID) - ignored, missing or not.
+    """
+
+    passports = parse(stdin)
+
+    patterns = {
+        k: re.compile(v) for k, v in {
+            "byr": r"19[2-9][0-9]|200[0-2]",
+            "iyr": r"201[0-9]|2020",
+            "eyr": r"202[0-9]|2030",
+            "hgt": r"1([5-8][0-9]|9[0-3])cm|(59|6[0-9]|7[0-6])in",
+            "hcl": r"#[0-9a-f]{6}",
+            "ecl": r"amb|blu|brn|gry|grn|hzl|oth",
+            "pid": r"[0-9]{9}",
+        }.items()
+    }
+
+    def validate(passport):
+        for key, pattern in patterns.items():
+            if key not in passport:
+                stderr.write(f"Missing {key}\n")
+                return False
+
+            if not pattern.fullmatch(passport[key]):
+                stderr.write(f"Invalid {key}\n")
+                return False
+
+        stderr.write("Valid\n")
+        return True
+
+    valid_passports = 0
     for passport in passports:
         stderr.write(f"{passport}\n")
         if validate(passport):
