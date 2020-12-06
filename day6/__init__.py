f484c0f567eda4fc0a2650b1b807ca77f22b2038
Mikkel Paulson <git@email.mikkel.ca>
2020-12-06 11:13:36 -0500

    day 6 part 2

--- day6/__init__.py
+++ day6/__init__.py
@@ -9,21 +9,35 @@ def part1(stdin, stdout, stderr):
 
     groups = parse(stdin)
     totals = [
-        len(group) for group in groups
+        len(set.union(*group)) for group in groups
     ]
 
     stderr.write(f"{totals}\n")
     stdout.write(f"{sum(totals)}\n")
 
 
+def part2(stdin, stdout, stderr):
+    """
+    For each group, count the number of questions to which everyone answered
+    "yes". What is the sum of those counts?
+    """
+
+    groups = parse(stdin)
+    intersections = [
+        len(set.intersection(*group)) for group in groups
+    ]
+
+    stderr.write(f"{intersections}\n")
+    stdout.write(f"{sum(intersections)}\n")
+
+
 def parse(stdin):
     """Parse a raw declaration form input."""
 
     return [
-        set(
-            answer
+        [
+            set(answer for answer in person)
             for person in group.split("\n")
-            for answer in person
-        )
+        ]
         for group in stdin.read().strip().split("\n\n")
     ]
