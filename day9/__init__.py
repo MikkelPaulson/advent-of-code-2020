ce49a20b9e831f8ef049f102b62ccd150070a41d
Mikkel Paulson <git@email.mikkel.ca>
2020-12-09 13:18:42 -0500

    day 9 part 2

--- day9/__init__.py
+++ day9/__init__.py
@@ -1,4 +1,4 @@
-"""https://adventofcode.com/2020/day/8"""
+"""https://adventofcode.com/2020/day/9"""
 
 
 def part1(stdin, stderr, preamble_size=25):
@@ -9,20 +9,41 @@ def part1(stdin, stderr, preamble_size=25):
     this property?
     """
 
-    def is_valid(value, preamble):
-        for i, _ in enumerate(preamble):
-            for j in range(i, len(preamble)):
-                if preamble[i] + preamble[j] == value:
-                    stderr.write(f"{preamble[i]} + {preamble[j]} = {value}\n")
-                    return True
-        return False
+    data = parse(stdin)
+    return str(get_invalid(data, stderr, preamble_size))
+
+
+def part2(stdin, stderr, preamble_size=25):
+    """
+    The final step in breaking the XMAS encryption relies on the invalid number
+    you just found: you must find a contiguous set of at least two numbers in
+    your list which sum to the invalid number from step 1.
+    """
 
     data = parse(stdin)
-    for i in range(preamble_size, len(data)):
-        if not is_valid(data[i], data[i - preamble_size:i]):
-            return str(data[i])
 
-    raise Exception("No invalid values found.")
+    target = get_invalid(data, stderr, preamble_size)
+    window_start = 0
+    window_end = 2
+    window_sum = sum(data[window_start:window_end])
+
+    while window_end < len(data):
+        stderr.write(f"{data[window_start:window_end]}")
+
+        if window_sum < target or window_end - window_start <= 2:
+            stderr.write(f" < {target}\n")
+            window_sum += data[window_end]
+            window_end += 1
+        elif window_sum > target:
+            stderr.write(f" > {target}\n")
+            window_sum -= data[window_start]
+            window_start += 1
+        else:
+            stderr.write(f" = {target}\n")
+            return str(min(data[window_start:window_end]) +
+                       max(data[window_start:window_end]))
+
+    raise Exception("No matches found.")
 
 
 def parse(stdin):
@@ -33,3 +54,19 @@ def parse(stdin):
     return [
         int(line) for line in stdin.read().strip().split("\n")
     ]
+
+
+def get_invalid(data, stderr, preamble_size=25):
+    def is_valid(value, preamble):
+        for i, _ in enumerate(preamble):
+            for j in range(i, len(preamble)):
+                if preamble[i] + preamble[j] == value:
+                    stderr.write(f"{preamble[i]} + {preamble[j]} = {value}\n")
+                    return True
+        return False
+
+    for i in range(preamble_size, len(data)):
+        if not is_valid(data[i], data[i - preamble_size:i]):
+            return data[i]
+
+    raise Exception("No invalid values found.")
