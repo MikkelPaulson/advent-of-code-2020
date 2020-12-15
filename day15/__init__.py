1cb8a7ee058bcff8ec9df1ab2a68a457ec23c859
Mikkel Paulson <git@email.mikkel.ca>
2020-12-15 11:47:03 -0500

    day 15 part 2

--- day15/__init__.py
+++ day15/__init__.py
@@ -8,26 +8,50 @@ def part1(stdin: io.TextIOWrapper, stderr: io.TextIOWrapper) -> str:
     Given your starting numbers, what will be the 2020th number spoken?
     """
 
-    numbers = parse(stdin)
-    stderr.write(f"{numbers}\n")
+    return nth(stdin, stderr, 2020)
 
+
+def part2(stdin: io.TextIOWrapper, stderr: io.TextIOWrapper) -> str:
+    """
+    Given your starting numbers, what will be the 30000000th number spoken?
+    """
+
+    return nth(stdin, stderr, 30000000)
+
+
+def nth(stdin: io.TextIOWrapper, stderr: io.TextIOWrapper, end: int) -> str:
+    """
+    Brute-force the nth number spoken given an input set of numbers.
+    """
+
+    debug_interval = end // 100
+    numbers = parse(stdin)
     number = next(reversed(numbers.keys()))
 
-    for turn in range(len(numbers), 2020):
+    for turn in range(len(numbers), end):
         (prev, last) = numbers.get(number)
 
-        stderr.write(f"{turn}: {number} was previously spoken in turn {prev}")
+        if turn % debug_interval == 0:
+            stderr.write(
+                f"{turn}: {number} was previously spoken in turn {prev}"
+            )
 
         if prev is None:
             number = 0
         else:
             number = last - prev
 
-        stderr.write(f", so I say \"{number}\"\n")
+        if turn % debug_interval == 0:
+            stderr.write(f", so I say \"{number}\"\n")
+
+        if prev is None:
+            number = 0
+        else:
+            number = last - prev
 
         numbers[number] = (numbers.get(number, (None, None))[1], turn)
 
-    return str(number)
+    return number
 
 
 def parse(stdin: io.TextIOWrapper) -> list:
