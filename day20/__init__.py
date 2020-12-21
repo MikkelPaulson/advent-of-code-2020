42571d9c437d316af1be2826684c96c5a1521299
Mikkel Paulson <git@email.mikkel.ca>
2020-12-21 12:48:44 -0500

    day 20 part 2 (slightly cleaner) progress

--- day20/__init__.py
+++ day20/__init__.py
@@ -1,7 +1,6 @@
 """https://adventofcode.com/2020/day/20"""
 
 import collections
-import functools
 import io
 import re
 
@@ -15,16 +14,10 @@ def part1(stdin: io.TextIOWrapper, stderr: io.TextIOWrapper) -> str:
     tiles = parse(stdin)
     stderr.write(f"tiles: {tiles}\n")
 
-    tile_sides = calc_sides(tiles)
-    stderr.write(f"tile_sides: {tile_sides}\n")
+    grid = compute_grid(tiles)
+    stderr.write(f"grid: {grid}\n")
 
-    vertices = get_vertices(tile_sides)
-    stderr.write(f"vertices: {vertices}\n")
-
-    corners = [vertex for vertex, edges in vertices.items() if len(edges) == 2]
-    stderr.write(f"corners: {corners}\n")
-
-    return str(functools.reduce(lambda a, b: a * b, corners))
+    return str(grid[0][0] * grid[0][-1] * grid[-1][0] * grid[-1][-1])
 
 
 def part2(stdin: io.TextIOWrapper, stderr: io.TextIOWrapper) -> str:
@@ -35,115 +28,103 @@ def part2(stdin: io.TextIOWrapper, stderr: io.TextIOWrapper) -> str:
     tiles = parse(stdin)
     stderr.write(f"tiles: {tiles}\n")
 
-    tile_sides = calc_sides(tiles)
-    stderr.write(f"tile_sides: {tile_sides}\n")
-
-    vertices = get_vertices(tile_sides)
-    stderr.write(f"vertices: {vertices}\n")
-
-    used_vertices = set()
-    grid = [[fill_tile(vertices, list(), used_vertices, 0, 0)]]
-    used_vertices.add(grid[0][0])
-    side_length = 1
-    while True:
-        vertex = fill_tile(vertices, grid, used_vertices, side_length, 0)
-        used_vertices.add(vertex)
-        stderr.write(f"row 0, col {side_length}: {vertex}\n")
-        side_length += 1
-        grid[0].append(vertex)
-        if len(vertices[vertex]) == 2:
-            break
-
-    for row in range(1, side_length):
-        grid.append(list())
-        for col in range(side_length):
-            vertex = fill_tile(vertices, grid, used_vertices, col, row)
-            used_vertices.add(vertex)
-            grid[row].append(vertex)
-            stderr.write(f"row {row}, col {col}: {vertex}\n")
-
+    grid = compute_grid(tiles)
     stderr.write(f"grid: {grid}\n")
 
-
-def fill_tile(vertices: dict, grid: list, used_vertices: set, col: int, row: int) -> int:
-
-    # top row
-    if row == 0:
-
-        # top left corner
-        if col == 0:
-            for vertex, edges in vertices.items():
-                if len(edges) == 2:
-                    return vertex
-
-            raise Exception("No top left corner vertex found.")
-
-        for vertex in vertices[grid[row][col - 1]]:
-            if len(vertices[vertex]) < 4 and vertex not in used_vertices:
-                return vertex
-
-        raise Exception("No top vertex found.")
-
-    side_length = len(grid[0])
-
-    # bottom row
-    if row == side_length - 1:
-
-        # bottom left corner
-        if col == 0:
-            for vertex in vertices[grid[row - 1][col]]:
-                if len(vertices[vertex]) == 2:
-                    return vertex
-
-            raise Exception("No bottom left corner vertex found.")
-
-        for vertex in vertices[grid[row][col - 1]]:
-            if len(vertices[vertex]) < 4 and vertex not in used_vertices:
-                return vertex
-
-        raise Exception("No bottom vertex found.")
-
-    # left/right side
-    if col in (0, side_length - 1):
-        for vertex in vertices[grid[row - 1][col]]:
-            if len(vertices[vertex]) < 4 and vertex not in used_vertices:
-                return vertex
-
-        raise Exception("No left/right vertex found.")
-
-    # non-side tile: The tile above ours and the tile to the left of ours will
-    # always have two tiles in common, ours and the one diagonally up and to
-    # the left of ours. We can look up its ID, so adding it to the mix means
-    # that its ID will appear three times among the vertices and ours twice.
-    # Taking the second most common result in the bunch gives us the target
-    # tile.
-    _, (vertex, _) = collections.Counter(
-        list(vertices[grid[row - 1][col]]) +
-        list(vertices[grid[row][col - 1]]) +
-        [grid[row - 1][col - 1]]
-    ).most_common(2)
-
-    return vertex
+    stderr.write(
+        f"corners: {grid[0][0]}, {grid[0][-1]}, "
+        f"{grid[-1][0]}, {grid[-1][-1]}\n"
+    )
 
 
-def get_vertices(tile_sides: dict) -> dict:
-    """Find matching edges within a set of tiles."""
+def compute_grid(tiles: dict) -> list:
+    """Compute the grid arrangement based on a set of tiles."""
 
-    edges = collections.defaultdict(list)
+    def match_edges(tiles: dict) -> dict:
+        sides = collections.defaultdict(list)
 
-    for tile_id, tile in tile_sides.items():
-        for edge in tile:
-            edges[edge].append(tile_id)
-            edges["".join(reversed(edge))].append(tile_id)
+        for tile_id, tile in calc_sides(tiles).items():
+            for side in tile:
+                sides[side].append(tile_id)
+                sides["".join(reversed(side))].append(tile_id)
 
-    vertices = collections.defaultdict(set)
+        edges = collections.defaultdict(set)
 
-    for edge in edges.values():
-        if len(edge) == 2:
-            vertices[edge[0]].add(edge[1])
-            vertices[edge[1]].add(edge[0])
+        for side in sides.values():
+            if len(side) == 2:
+                edges[side[0]].add(side[1])
+                edges[side[1]].add(side[0])
 
-    return dict(vertices.items())
+        return dict(edges.items())
+
+    def arrange_vertices(edges: dict) -> list:
+        def find_corner(edges: dict) -> int:
+            for vertex_id, vertex_edges in edges.items():
+                if len(vertex_edges) == 2:
+                    return vertex_id
+
+            raise Exception("No corner vertex found.")
+
+        def get_common_vertices(edges: tuple) -> set:
+            result = set()
+            counter = collections.Counter([
+                vertex
+                for vertices in edges
+                for vertex in vertices
+            ])
+            for vertex, count in counter.most_common():
+                if count == 1:
+                    return result
+
+                result.add(vertex)
+
+            return result
+
+        grid = [[find_corner(edges)]]
+        line_length = None
+        used_vertices = {grid[0][0]}
+        col, row = 0, 0
+        while len(used_vertices) < len(edges):
+            col += 1
+            if col == line_length:
+                col = 0
+                row += 1
+                grid.append(list())
+
+            try:
+                common_vertices = get_common_vertices((
+                    edges[grid[row - 1][col]],
+                    edges[grid[row][col - 1]],
+                ))
+
+                for vertex in common_vertices:
+                    if vertex not in used_vertices:
+                        used_vertices.add(vertex)
+                        grid[row].append(vertex)
+                        break
+                else:
+                    raise Exception("No common vertex found.")
+
+            except IndexError:
+                if col == 0:
+                    common_vertices = edges[grid[row - 1][col]]
+                else:
+                    common_vertices = edges[grid[row][col - 1]]
+
+                for vertex in common_vertices:
+                    if vertex not in used_vertices and len(edges[vertex]) < 4:
+                        if line_length is None and len(edges[vertex]) == 2:
+                            line_length = col + 1
+
+                        used_vertices.add(vertex)
+                        grid[row].append(vertex)
+                        break
+                else:
+                    raise Exception("No edge vertex found.")
+
+        return grid
+
+    return arrange_vertices(match_edges(tiles))
 
 
 def calc_sides(tiles: dict) -> dict:
