c5ffd89e34507626ed1977c7f3005739b8b7fa17
Mikkel Paulson <git@email.mikkel.ca>
2020-12-21 18:07:32 -0500

    day 20 part 2!

--- day20/__init__.py
+++ day20/__init__.py
@@ -31,10 +31,158 @@ def part2(stdin: io.TextIOWrapper, stderr: io.TextIOWrapper) -> str:
     grid = compute_grid(tiles)
     stderr.write(f"grid: {grid}\n")
 
-    stderr.write(
-        f"corners: {grid[0][0]}, {grid[0][-1]}, "
-        f"{grid[-1][0]}, {grid[-1][-1]}\n"
-    )
+    image = assemble_image(tiles, grid)
+    sea_monsters = count_sea_monsters(image)
+
+    return str(len([
+        char
+        for line in image
+        for char in line if char == '#'
+    ]) - sea_monsters * 15)
+
+
+def assemble_image(tiles: dict, grid: list) -> list:
+    """Assemble the set of tiles into a finished image."""
+
+    def rotate_tile(tile: list) -> list:
+        return [
+            "".join([
+                tile[col][len(tile) - row - 1]
+                for col, _ in enumerate(tile)
+            ])
+            for row, _ in enumerate(tile[0])
+        ]
+
+    def align_tiles(tiles: dict, grid: list) -> list:
+        def align_first_tile(tiles: tuple, grid: list) -> list:
+            """Initialize the first tile."""
+            tile = tiles[grid[0][0]].copy()
+            tile_sides = calc_sides(tile)
+
+            tile_sides_set = set(tile_sides)
+            adjacent_sides = list()
+            for adjacent_tile in [tiles[grid[0][1]], tiles[grid[1][0]]]:
+
+                adjacent_tile_sides = set(calc_sides(adjacent_tile))
+
+                if tile_sides_set.isdisjoint(adjacent_tile_sides):
+                    adjacent_tile.reverse()
+                    adjacent_sides.append((
+                        set(calc_sides(adjacent_tile))
+                        & tile_sides_set
+                    ).pop())
+                else:
+                    adjacent_sides.append((
+                        adjacent_tile_sides
+                        & tile_sides_set
+                    ).pop())
+
+            # turns out everything was backwards, flip it
+            if (tile_sides.index(adjacent_sides[0]) !=
+                    (tile_sides.index(adjacent_sides[1]) + 1) % 4):
+                tile.reverse()
+                tile_sides = calc_sides(tile)
+                adjacent_sides = ["".join(reversed(side))
+                                  for side in adjacent_sides]
+
+            # Re-align the tile with the right-adjacent side on the right (W)
+            return align_tile(tile, adjacent_sides[0], 1)
+
+        def align_tile(tile: list, side: str, position: int) -> list:
+            """Align a tile so that the side at position == side."""
+            tile = tile.copy()
+            tile_sides = calc_sides(tile)
+            side = "".join(reversed(side))
+
+            if side not in tile_sides:
+                tile.reverse()
+                tile_sides = calc_sides(tile)
+
+            if side not in tile_sides:
+                raise Exception(f"{side} not found in {tile}")
+
+            while tile_sides[position] != side:
+                tile = rotate_tile(tile)
+                tile_sides = calc_sides(tile)
+
+            return tile
+
+        aligned_tiles = list()
+        for row, tile_ids in enumerate(grid):
+            aligned_tiles.append(list())
+            for col, tile_id in enumerate(tile_ids):
+                if row == 0 and col == 0:
+                    aligned_tiles[row].append(align_first_tile(tiles, grid))
+                elif row == 0:
+                    aligned_tiles[row].append(align_tile(
+                        tiles[tile_id],
+                        calc_sides(aligned_tiles[row][col - 1])[1],
+                        3
+                    ))
+                else:
+                    aligned_tiles[row].append(align_tile(
+                        tiles[tile_id],
+                        calc_sides(aligned_tiles[row - 1][col])[2],
+                        0
+                    ))
+
+        return aligned_tiles
+
+    def join_tiles(grid: list) -> list:
+        return [
+            "".join([
+                tile[line_num][1:-1]
+                for tile in tile_row
+            ])
+            for tile_row in grid
+            for line_num in range(1, len(tile_row[0]) - 1)
+        ]
+
+    aligned_tiles = align_tiles(tiles, grid)
+
+    image = join_tiles(aligned_tiles)
+    if count_sea_monsters(image) > 0:
+        return image
+
+    for _ in range(3):
+        image = rotate_tile(image)
+        if count_sea_monsters(image) > 0:
+            return image
+
+    image.reverse()
+    if count_sea_monsters(image) > 0:
+        return image
+
+    for _ in range(3):
+        image = rotate_tile(image)
+        if count_sea_monsters(image) > 0:
+            return image
+
+    raise Exception("No sea monsters found in any orientation.")
+
+
+#   01234567890123456789
+# 0 ..................#.
+# 1 #....##....##....###
+# 2 .#..#..#..#..#..#...
+SEA_MONSTER = set([(1, 0), (2, 1), (2, 4), (1, 5), (1, 6),
+                   (2, 7), (2, 10), (1, 11), (1, 12), (2, 13),
+                   (2, 16), (1, 17), (1, 18), (0, 18), (1, 19)])
+SEA_MONSTER_WIDTH = 20
+SEA_MONSTER_HEIGHT = 3
+
+
+def count_sea_monsters(image: list) -> int:
+    """Count the number of sea monsters identified in the image."""
+    count = 0
+    for row in range(len(image) - SEA_MONSTER_HEIGHT):
+        for col in range(len(image[0]) - SEA_MONSTER_WIDTH):
+            for row_offset, col_offset in SEA_MONSTER:
+                if image[row + row_offset][col + col_offset] != "#":
+                    break
+            else:
+                count += 1
+    return count
 
 
 def compute_grid(tiles: dict) -> list:
@@ -43,8 +191,8 @@ def compute_grid(tiles: dict) -> list:
     def match_edges(tiles: dict) -> dict:
         sides = collections.defaultdict(list)
 
-        for tile_id, tile in calc_sides(tiles).items():
-            for side in tile:
+        for tile_id, tile in tiles.items():
+            for side in calc_sides(tile):
                 sides[side].append(tile_id)
                 sides["".join(reversed(side))].append(tile_id)
 
@@ -127,17 +275,15 @@ def compute_grid(tiles: dict) -> list:
     return arrange_vertices(match_edges(tiles))
 
 
-def calc_sides(tiles: dict) -> dict:
+def calc_sides(tile: list) -> list:
     """Compute the sides of each tile, returned in a N W S E order."""
 
-    return {
-        tile_id: [
-            tile[0],
-            "".join([line[-1] for line in tile]),
-            "".join(reversed(tile[-1])),
-            "".join([line[0] for line in reversed(tile)]),
-        ] for tile_id, tile in tiles.items()
-    }
+    return [
+        tile[0],
+        "".join([line[-1] for line in tile]),
+        "".join(reversed(tile[-1])),
+        "".join([line[0] for line in reversed(tile)]),
+    ]
 
 
 def parse(stdin: io.TextIOWrapper) -> dict:
