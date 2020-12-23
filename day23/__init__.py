08c7af31921c1f5e7965c96a4c0e33be5ffe5b69
Mikkel Paulson <git@email.mikkel.ca>
2020-12-23 11:55:38 -0500

    day 23: refactor playing rounds into method

--- day23/__init__.py
+++ day23/__init__.py
@@ -11,15 +11,7 @@ def part1(stdin: io.TextIOWrapper, stderr: io.TextIOWrapper) -> int:
 
     cups = parse(stdin)
 
-    current_cup = cups[0]
-
-    for round_num in range(100):
-        stderr.write(
-            f"move {round_num + 1}: " + " ".join(map(str, cups)).replace(
-                str(current_cup), f"({current_cup})"
-            ) + "\n"
-        )
-        current_cup = play_round(cups, current_cup)
+    play(cups, 100)
 
     stderr.write(f"End of game: {cups}\n")
 
@@ -30,29 +22,40 @@ def part1(stdin: io.TextIOWrapper, stderr: io.TextIOWrapper) -> int:
     ))
 
 
-def play_round(cups: list, current_cup: int) -> int:
+def play(cups: list, rounds: int):
     """
-    Play a round of the cup game. Returns the new current cup.
+    Play the game for a specified number of rounds. Modifies cups in place.
     """
 
-    cups_in_hand = list()
-    for _ in range(3):
-        cups_in_hand.append(cups.pop(
-            (cups.index(current_cup) + 1) % len(cups)))
+    def play_round(cups: list, current_cup: int) -> int:
+        """
+        Play a round of the cup game. Returns the new current cup.
+        """
 
-    destination_cup = current_cup - 1
-    while destination_cup > 0:
-        try:
-            destination_index = cups.index(destination_cup)
-            break
-        except ValueError:
-            destination_cup -= 1
-    else:
-        destination_index = cups.index(max(cups))
+        i = cups.index(current_cup)
+        cups_in_hand = cups[i+1:i+4]
+        del cups[i+1:i+4]
 
-    cups[destination_index + 1:destination_index + 1] = cups_in_hand
+        while len(cups_in_hand) < 3:
+            cups_in_hand.append(cups.pop(0))
 
-    return cups[(cups.index(current_cup) + 1) % len(cups)]
+        destination_cup = current_cup - 1
+        while destination_cup > 0:
+            try:
+                destination_index = cups.index(destination_cup)
+                break
+            except ValueError:
+                destination_cup -= 1
+        else:
+            destination_index = cups.index(max(cups))
+
+        cups[destination_index + 1:destination_index + 1] = cups_in_hand
+
+        return cups[(cups.index(current_cup) + 1) % len(cups)]
+
+    current_cup = cups[0]
+    for _ in range(rounds):
+        current_cup = play_round(cups, current_cup)
 
 
 def parse(stdin: io.TextIOWrapper) -> list:
