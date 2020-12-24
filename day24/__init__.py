32fd08f9cb78ed2a38b4536e597df1a57bb15a7f
Mikkel Paulson <git@email.mikkel.ca>
2020-12-24 13:49:28 -0500

    day 24 part 2

--- day24/__init__.py
+++ day24/__init__.py
@@ -4,24 +4,57 @@ import functools
 import io
 
 
-def part1(stdin: io.TextIOWrapper, stderr: io.TextIOWrapper) -> int:
+def part1(stdin: io.TextIOWrapper, _stderr: io.TextIOWrapper) -> int:
     """
     Go through the renovation crew's list and determine which tiles they need
     to flip. After all of the instructions have been followed, how many tiles
     are left with the black side up?
     """
+    return len(parse_and_evaluate(stdin))
 
-    flips = [locate_tile(directions) for directions in parse(stdin)]
-    stderr.write(f"{flips}\n")
 
-    tiles = set()
-    for flip in flips:
-        if flip in tiles:
-            tiles.remove(flip)
-        else:
-            tiles.add(flip)
+def part2(stdin: io.TextIOWrapper, stderr: io.TextIOWrapper) -> int:
+    """
+    How many tiles will be black after 100 days?
+    """
 
-    stderr.write(f"{tiles}\n")
+    def get_min_coord(a_coord: tuple, b_coord: tuple) -> tuple:
+        return tuple(map(min, zip(a_coord, b_coord)))
+
+    def get_max_coord(a_coord: tuple, b_coord: tuple) -> tuple:
+        return tuple(map(max, zip(a_coord, b_coord)))
+
+    def tile_generation(tiles: set, min_coord: tuple, max_coord: tuple) -> \
+            (set, tuple, tuple):
+        next_generation = set()
+        for x_pos in range(min_coord[0] - 1, max_coord[0] + 2):
+            for y_pos in range(min_coord[1] - 1, max_coord[1] + 2):
+                adjacent = count_adjacent(tiles, (x_pos, y_pos))
+
+                if adjacent == 2 or (adjacent == 1 and
+                                     (x_pos, y_pos) in tiles):
+                    next_generation.add((x_pos, y_pos))
+                    min_coord = get_min_coord(min_coord, (x_pos, y_pos))
+                    max_coord = get_max_coord(max_coord, (x_pos, y_pos))
+
+        return next_generation, min_coord, max_coord
+
+    def count_adjacent(tiles: set, coord: tuple) -> int:
+        count = 0
+        for direction in TILE_DIRECTIONS:
+            if locate_tile([direction], coord) in tiles:
+                count += 1
+        return count
+
+    tiles = parse_and_evaluate(stdin)
+
+    min_coord = functools.reduce(get_min_coord, tiles, (0, 0))
+    max_coord = functools.reduce(get_max_coord, tiles, (0, 0))
+
+    for generation in range(0, 100):
+        stderr.write(f"Day {generation}: {len(tiles)}\n")
+        tiles, min_coord, max_coord = \
+            tile_generation(tiles, min_coord, max_coord)
 
     return len(tiles)
 
@@ -41,7 +74,7 @@ TILE_DIRECTIONS = {
 }
 
 
-def locate_tile(directions: list) -> tuple:
+def locate_tile(directions: list, start: tuple = (0, 0)) -> tuple:
     """
     Given a set of directions, determine the coordinates of a tile.
     """
@@ -52,28 +85,45 @@ def locate_tile(directions: list) -> tuple:
     return functools.reduce(
         lambda a, b: tuple(map(sum, zip(a, b))),
         mapped_directions,
-        (0, 0)
+        start
     )
 
 
-def parse(stdin: io.TextIOWrapper) -> list:
+def parse_and_evaluate(stdin: io.TextIOWrapper) -> set:
     """
-    Parse the input into a list of lists of strings, each string being a
-    direction: "e", "se", "sw", "w", "nw", and "ne". Each list refers to a
-    single tile to flip.
+    Parse the input stream and reduce it to a set of coordinates representing
+    the black tiles.
     """
 
-    def parse_line(line: str) -> list:
-        result = list()
-        i = 0
-        while i < len(line):
-            if line[i] in ["n", "s"]:
-                result.append(line[i:i + 2])
-                i += 2
-            else:
-                result.append(line[i])
-                i += 1
+    def parse(stdin: io.TextIOWrapper) -> list:
+        """
+        Parse the input into a list of lists of strings, each string being a
+        direction: "e", "se", "sw", "w", "nw", and "ne". Each list refers to a
+        single tile to flip.
+        """
 
-        return result
+        def parse_line(line: str) -> list:
+            result = list()
+            i = 0
+            while i < len(line):
+                if line[i] in ["n", "s"]:
+                    result.append(line[i:i + 2])
+                    i += 2
+                else:
+                    result.append(line[i])
+                    i += 1
 
-    return [parse_line(line) for line in stdin.read().strip().splitlines()]
+            return result
+
+        return [parse_line(line) for line in stdin.read().strip().splitlines()]
+
+    flips = [locate_tile(directions) for directions in parse(stdin)]
+
+    tiles = set()
+    for flip in flips:
+        if flip in tiles:
+            tiles.remove(flip)
+        else:
+            tiles.add(flip)
+
+    return tiles
